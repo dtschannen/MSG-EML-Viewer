aca3c086be1e0be4385b698c4ff63d27eeb501da
dtschannen <dario@dariotschannen.ch>
2025-12-28 02:34:39 +0100

    add g tag

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Buffer } from 'buffer';
+import TagManager from 'react-gtm-module';
 import App from './App.tsx';
 import './index.css';
 
 window.Buffer = Buffer;
+
+// Initialize Google Tag Manager
+const tagManagerArgs = {
+  gtmId: 'GTM-MN96DX79'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
